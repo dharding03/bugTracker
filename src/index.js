e162d6d7f3622c40292c6c8aac262b0f06638580
Daniel Harding <dan.harding@myemail.indwes.edu>
2021-08-18 12:21:35 -0400

    Project startup

--- src/index.js
+++ src/index.js
@@ -2,16 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {configureStore, combineReducers} from "@reduxjs/toolkit";
+import authSlice from "./Controllers/Redux/authReducer";
+import bugSlice from "./Controllers/Redux/bugReducer";
+import userSlice from "./Controllers/Redux/userReducer";
+
+const reducer = combineReducers({
+    auth: authSlice,
+    bug: bugSlice,
+    user: userSlice
+})
+
+const store = configureStore({
+    reducer
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+    <App />,
+    </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
